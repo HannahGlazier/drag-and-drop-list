daf6dfeb328eefbba249086dcf08dfe078cee7f3
Hannah Glazier <hrayglazier@aol.com>
2022-05-25 09:54:23 -0600

    initial dnd testing/setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,55 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import React, { useState, useEffect } from "react";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-// Internal imports
-import RoomContainer from "./RoomContainer"
+// // Internal imports
+// import RoomContainer from "./RoomContainer"
+// import TestData from './TestData'
 
-// dnd
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+// // dnd
+// import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-function App() {
-  const [rooms, setRooms] = useState([])
+// function App() {
+//   const [rooms, setRooms] = useState([])
+//   state = TestData
 
-  // TEST FETCHES
-  useEffect(() => {
-    fetch('/rooms')
-    .then(response => response.json())
-    .then(rooms => setRooms(rooms))
-  }, [])
+//   // TEST FETCHES
+//   useEffect(() => {
+//     fetch('/rooms')
+//     .then(response => response.json())
+//     .then(rooms => setRooms(rooms))
+//   }, [])
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-          TESTING APP 
-      </header>
-      <RoomContainer />
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//           TESTING APP 
+//       </header>
+//       <RoomContainer />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import '@atlaskit/css-reset';
+import TestData from './TestData';
+import Column from './column';
+
+class App extends React.Component {
+  state = TestData;
+
+  render() {
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+      return <Column key={column.id} column={column} tasks={tasks} />;
+    });
+  }
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App
